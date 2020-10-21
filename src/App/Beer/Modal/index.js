12444195e6e7fcdc36eee16c76ad75b8c2556970
Pita <itamarga@gmail.com>
2020-10-21 20:43:56 +0300

    Improve modal styling and layout, add food recommendation generator

--- src/App/Beer/Modal/index.js
+++ src/App/Beer/Modal/index.js
@@ -14,19 +14,47 @@ const DialogContent = withStyles((theme) => ({
   },
 }))(MuiDialogContent);
 
+const DialogTitle = withStyles(() => ({
+  root: {
+    // padding: theme.spacing(2),
+    backgroundColor: "#333",
+    color: "white",
+    textAlign: "center",
+    boxShadow: "0px 0px 1px white inset",
+  },
+}))(MuiDialogTitle);
+
 function Modal(props) {
+  const getFoodRecommendation = (foodArray) => {
+    const foodPairing = [...foodArray];
+    foodPairing.splice(-1, 0, "or");
+    const foodPairingString = foodPairing
+      .map((f) => (f === "or" ? "or" : `${f},`))
+      .join(" ");
+    const openings = [
+      "Best with",
+      "Enhanced by ",
+      "Try it with ",
+      "Tastes great with ",
+      "Recommended alongside",
+      "Goes hand in hand with",
+    ];
+    const randomIndex = Math.floor(Math.random() * openings.length);
+    const randomOpening = openings[randomIndex];
+    return `${randomOpening} ${foodPairingString}`.slice(0, -1);
+  };
   return (
     <Dialog
       onClose={props.handleModalClose}
       aria-labelledby="customized-dialog-title"
       open={props.modalOpen}
     >
-      <MuiDialogTitle
+      <DialogTitle
         id="customized-dialog-title"
         onClose={props.handleModalClose}
       >
         {props.beer.name}
-      </MuiDialogTitle>
+      </DialogTitle>
       <DialogContent dividers>
         <div className="dialog-content-div">
           <img
@@ -42,12 +70,11 @@ function Modal(props) {
               {props.beer.description}
             </Typography>
             <Typography gutterBottom className="dialog-food">
-              {props.beer.food_pairing}
+              {getFoodRecommendation(props.beer.food_pairing)}
             </Typography>
-            <Typography
-              gutterBottom
-              className="dialog-first-brewed"
-            >{`First brewed: ${props.beer.first_brewed}`}</Typography>
+            <Typography gutterBottom className="dialog-alcohol">
+              {`Alcohol: ${props.beer.abv}%`}
+            </Typography>
           </div>
         </div>
       </DialogContent>
